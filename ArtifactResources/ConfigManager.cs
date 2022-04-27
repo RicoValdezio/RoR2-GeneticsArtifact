d55969dc3d03a0f0365dec137f90c3f35e4c78bd
Kyler Akers <kylera14@gmail.com>
2022-04-27 14:50:49 -0500

    Added config option and mode flag to keep applying stat mods even if the artifact is disabled, new bodies will copy from master without mutating.

--- ArtifactResources/ConfigManager.cs
+++ ArtifactResources/ConfigManager.cs
@@ -12,11 +12,14 @@ namespace GeneticsArtifact
 
         public static ConfigEntry<float> geneVarianceLimit, geneCap, geneFloor, geneProductLimit;
 
+        public static ConfigEntry<bool> maintainIfDisabled;
+
         public static void Init(ConfigFile configFile)
         {
             governorType = configFile.Bind<int>(new ConfigDefinition("GeneEngineDriver Variables", "Learning Governor Type"), 0, new ConfigDescription("How the algorithm decides when to learn: 0 - Default, 1 - Time Only, 2 - Death Count Only", new AcceptableValueRange<int>(0, 2)));
             timeLimit = configFile.Bind<int>(new ConfigDefinition("GeneEngineDriver Variables", "Time Limit"), 60, new ConfigDescription("How many seconds between learnings:", new AcceptableValueRange<int>(5, 300))); // 5 seconds to 5 minutes
             deathLimit = configFile.Bind<int>(new ConfigDefinition("GeneEngineDriver Variables", "Death Limit"), 40, new ConfigDescription("How many monster deaths between learnings:", new AcceptableValueRange<int>(10, 100)));
+            maintainIfDisabled = configFile.Bind<bool>(new ConfigDefinition("GeneEngineDriver Variables", "Keep Mutations While Disabled"), false, new ConfigDescription("Should the stat mods still be applied if the artifact is disabled mid-run:", new AcceptableValueList<bool>(true, false)));
 
             geneCap = configFile.Bind<float>(new ConfigDefinition("Mutation Variables", "Gene Value Cap"), 10.00f, new ConfigDescription("Maximum multiplier for any stat:", new AcceptableValueRange<float>(1f, 50f)));
             geneFloor = configFile.Bind<float>(new ConfigDefinition("Mutation Variables", "Gene Value Floor"), 0.01f, new ConfigDescription("Minimum multiplier for any stat:", new AcceptableValueRange<float>(0.01f, 1f)));
